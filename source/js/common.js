98137c096ef896e7b77b8f9aa22e0ecd07957a0c
Urii <urii.mykhailets@gmail.com>
2022-07-18 18:42:13 +0300

    v3 add gallery page

--- source/js/common.js
+++ source/js/common.js
@@ -29,6 +29,28 @@ let mainSlider = new Swiper(".index-first__swiper", {
 // })
 
 
+const select = document.querySelectorAll(".select");
+if (select.length) {
+  select.forEach((item) => {
+    const selectCurrent = item.querySelector(".select__current");
+    item.addEventListener("click", (event) => {
+      const el = event.target.dataset.choice;
+      const text = event.target.innerText;
+      if (el === "choosen" && selectCurrent.innerText !== text) {
+        selectCurrent.innerText = text;
+      }
+      item.classList.toggle("active");
+    });
+    document.addEventListener("click", function (event) {
+      if (!item.contains(event.target)) {
+        item.classList.remove("active");
+      }
+    });
+  });
+};
+
+
+
 
 
 
